ed0b4d759b8d33b6fa38a09ac481e634288061b7
nndang <nndang.sc@gmail.com>
2025-12-14 22:08:01 +0700

    feat(backend): cache db adapter

--- backend/go.mod
+++ backend/go.mod
@@ -16,6 +16,8 @@ require (
 	github.com/go-playground/validator/v10 v10.27.0
 	github.com/redis/go-redis/v9 v9.17.2
 	golang.org/x/time v0.14.0
+	gorm.io/driver/postgres v1.6.0
+	gorm.io/gorm v1.31.1
 )
 
 require (
@@ -30,6 +32,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- backend/internal/infrastructure/cache/kv_cache.go
+++ backend/internal/infrastructure/cache/kv_cache.go
@@ -1,106 +1,110 @@
 package cache
 
-// import (
-// 	"context"
-// 	"time"
-// )
+import (
+	"context"
+	"errors"
+	"time"
 
-// type kvcache struct {
-// 	redis *Redis
-// }
+	"github.com/redis/go-redis/v9"
+	"github.com/tommjj/chess_OG/backend/internal/core/domain"
+)
 
-// func NewKeyValueCacheAdapter(redis *Redis) *kvcache {
-// 	return &kvcache{
-// 		redis: redis,
-// 	}
-// }
+type kvcache struct {
+	redis *Redis
+}
 
-// func (r *kvcache) Set(ctx context.Context, key string, value []byte, ttl time.Duration) error {
-// 	return r.redis.Set(ctx, key, value, ttl).Err()
-// }
+func NewKeyValueCacheAdapter(redis *Redis) *kvcache {
+	return &kvcache{
+		redis: redis,
+	}
+}
 
-// // MSet is like Set but accepts multiple values:
-// //
-// //	MSet("key1", "value1", "key2", "value2")
-// func (r *kvcache) MSet(ctx context.Context, ttl time.Duration, kv map[string]interface{}) error {
-// 	pipe := r.redis.Pipeline()
+func (r *kvcache) Set(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return r.redis.Set(ctx, key, value, ttl).Err()
+}
 
-// 	pipe.MSet(ctx, kv)
+// MSet is like Set but accepts multiple values:
+//
+//	MSet("key1", "value1", "key2", "value2")
+func (r *kvcache) MSet(ctx context.Context, ttl time.Duration, kv map[string]interface{}) error {
+	pipe := r.redis.Pipeline()
 
-// 	if ttl > 0 {
-// 		for key := range kv {
-// 			pipe.Expire(ctx, key, ttl)
-// 		}
-// 	}
+	pipe.MSet(ctx, kv)
 
-// 	_, err := pipe.Exec(ctx)
-// 	return err
-// }
+	if ttl > 0 {
+		for key := range kv {
+			pipe.Expire(ctx, key, ttl)
+		}
+	}
 
-// func (r *kvcache) SetNX(ctx context.Context, key string, value []byte, ttl time.Duration) error {
-// 	return r.redis.SetEx(ctx, key, value, ttl).Err()
-// }
+	_, err := pipe.Exec(ctx)
+	return err
+}
 
-// func (r *kvcache) Get(ctx context.Context, key string) ([]byte, error) {
-// 	val, err := r.redis.Get(ctx, key).Result()
-// 	if err != nil {
-// 		return nil, handleRedisErr(err)
-// 	}
+func (r *kvcache) SetNX(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return r.redis.SetEx(ctx, key, value, ttl).Err()
+}
 
-// 	return []byte(val), nil
-// }
+func (r *kvcache) Get(ctx context.Context, key string) ([]byte, error) {
+	val, err := r.redis.Get(ctx, key).Result()
+	if err != nil {
+		return nil, handleRedisErr(err)
+	}
 
-// func (r *kvcache) MGet(ctx context.Context, key ...string) ([]any, error) {
-// 	vals, err := r.redis.MGet(ctx, key...).Result()
-// 	if err != nil {
-// 		return nil, handleRedisErr(err)
-// 	}
+	return []byte(val), nil
+}
 
-// 	return vals, nil
-// }
+func (r *kvcache) MGet(ctx context.Context, key ...string) ([]any, error) {
+	vals, err := r.redis.MGet(ctx, key...).Result()
+	if err != nil {
+		return nil, handleRedisErr(err)
+	}
 
-// func (r *kvcache) Exists(ctx context.Context, key string) (bool, error) {
-// 	exists, err := r.redis.Exists(ctx, key).Result()
-// 	if err != nil {
-// 		return false, err
-// 	}
+	return vals, nil
+}
 
-// 	return exists > 0, nil
-// }
+func (r *kvcache) Exists(ctx context.Context, key string) (bool, error) {
+	exists, err := r.redis.Exists(ctx, key).Result()
+	if err != nil {
+		return false, err
+	}
 
-// func (r *kvcache) DelByPrefix(ctx context.Context, prefix string) error {
-// 	var cursor uint64
-// 	var keys []string
+	return exists > 0, nil
+}
 
-// 	for {
-// 		var err error
-// 		keys, cursor, err = r.redis.Scan(ctx, cursor, prefix+"*", 100).Result()
-// 		if err != nil {
-// 			return err
-// 		}
+func (r *kvcache) DelByPrefix(ctx context.Context, prefix string) error {
+	var cursor uint64
+	var keys []string
 
-// 		for _, key := range keys {
-// 			err := r.redis.Del(ctx, key).Err()
-// 			if err != nil {
-// 				return err
-// 			}
-// 		}
+	for {
+		var err error
+		keys, cursor, err = r.redis.Scan(ctx, cursor, prefix+"*", 100).Result()
+		if err != nil {
+			return err
+		}
 
-// 		if cursor == 0 {
-// 			break
-// 		}
-// 	}
+		for _, key := range keys {
+			err := r.redis.Del(ctx, key).Err()
+			if err != nil {
+				return err
+			}
+		}
 
-// 	return nil
-// }
+		if cursor == 0 {
+			break
+		}
+	}
 
-// func (r *kvcache) Del(ctx context.Context, key string) error {
-// 	return r.redis.Del(ctx, key).Err()
-// }
+	return nil
+}
 
-// func handleRedisErr(err error) error {
-// 	if errors.Is(err, redis.Nil) {
-// 		return domain.ErrDataNotFound
-// 	}
-// 	return err
-// }
+func (r *kvcache) Del(ctx context.Context, key string) error {
+	return r.redis.Del(ctx, key).Err()
+}
+
+func handleRedisErr(err error) error {
+	if errors.Is(err, redis.Nil) {
+		return domain.ErrDataNotFound
+	}
+	return err
+}
